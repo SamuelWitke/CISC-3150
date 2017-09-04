20ba41f7328767e04c5125935a482d6765610848
SamuelWitke <SamuelWitke@gmail.com>
2017-09-04 13:37:45 -0400

    new file:   Point.class 	new file:   Question.class 	new file:   Question.java 	new file:   README.md 	new file:   TriangleSide.class

---
+++ Homework1/Question5/Question.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Point {
+	public int x;
+	public int y;
+	public Point(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Question {
+
+	public static double length(Point side1,Point side2){
+		int x1 = side1.x;
+		int x2 = side2.x;
+		int y1 = side1.y;
+		int y2 = side2.y;
+		return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));	
+	}
+	
+	public static boolean isReal(double triangle[]){
+		double a = triangle[0];
+		double b = triangle[1];
+		double c = triangle[2];
+		return !( (a + b <= c) || (a + c <= b) || (b + c <= a));
+	}
+
+	public static void main(String []args) {
+		Scanner input = new Scanner(System.in);
+		String line;
+		String[] lineArr;
+		Point points[] = new Point[3];
+		double triangle[] = new double[3];
+
+		System.out.println(" Enter 3 2D Points Seperated By Comma ex. 2,3");
+		for(int i=0;i<3;i++){
+			line = input.nextLine();
+			lineArr = line.split(",");
+			points[i] = new Point(
+								Integer.parseInt(lineArr[0]),Integer.parseInt(lineArr[1])
+							);
+		}
+		
+		triangle[0] = length(points[0],points[1]);
+		triangle[1] = length(points[0],points[2]);
+		triangle[2] = length(points[1],points[2]);
+		for(int i=0;i<3;i++){
+			System.out.printf("Length = %f \n",triangle[i]);
+		}
+		System.out.println(isReal(triangle) ? "Real" : "Not Real");
+	}
+}
---
+++ Homework1/Question5/README.md
@@ -0,0 +1,36 @@
+## Question 5:
+Write a program that reads three 2D points A, B, and C from the user. 
+Next, calculate the length of each side of the triangle using the 
+distance formula: sqrt((x2-x1)^2+(y2-y1)^2). Finally, determine if it's a 
+real triangle. For a triangle to be real, the sum of any two sides needs 
+to be greater than the third. Make sure your output has some real and at 
+least one fake triangle.
+
+## Output
+Enter 3 2D Points Seperated By Comma ex. 2,3    
+3,2  
+1,4  
+5,6  
+Length = 2.828427   
+Length = 4.472136   
+Length = 4.472136   
+Real  
+
+Enter 3 2D Points Seperated By Comma ex. 2,3  
+1,1  
+1,1  
+1,5  
+Length = 0.000000   
+Length = 4.000000   
+Length = 4.000000   
+Not Real  
+
+Enter 3 2D Points Seperated By Comma ex. 2,3  
+4,5  
+4,6  
+6,7  
+Length = 1.000000   
+Length = 2.828427   
+Length = 2.236068   
+Real  
+
