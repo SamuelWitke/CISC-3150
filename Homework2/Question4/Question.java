683ecd61ac3eb8c51e777c416147e6e60aba3525
SamuelWitke <SamuelWitke@gmail.com>
2017-09-10 20:11:17 -0400

    modified:   Question.class 	modified:   Question.java 	modified:   README.md

--- Homework2/Question4/Question.java
+++ Homework2/Question4/Question.java
@@ -4,5 +4,12 @@ public class Question {
 		System.out.printf("x>>32 = %32s\n",Integer.toBinaryString((x>>>32)));
 		System.out.printf("x>>0  = %32s\n",Integer.toBinaryString((x>>>0)));
 		System.out.printf("x     = %32s\n",Integer.toBinaryString((x)));
+		for(int i=0;i<35;i++){  
+			System.out.printf("%d\t\t%32s\n",i,Integer.toBinaryString(x>>>i));  
+		}  
+		for(int i=0;i<35;i++){  
+			System.out.printf("%d\t\t%32s\n",i,Integer.toBinaryString(x));  
+			x = x >>> i;  
+		}  
 	}
 }
